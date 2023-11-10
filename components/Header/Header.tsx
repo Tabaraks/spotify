0f2722da694e9003e31389d3d105021eb566ab3a
Tabarak Sohail <tabaraksohail96@gmail.com>
2023-11-11 00:48:40 +0500

    app-layout-complete

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,75 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "../Button";
 
 type HeaderProps = {
   children: React.ReactNode;
+  classname?: string;
 };
 
-const Header = ({ children }: HeaderProps) => {
-  return <div>{children}</div>;
+const Header = ({ children, classname }: HeaderProps) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    //Handle Logout
+  };
+
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        classname
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black text-center justify-center flex hover:opacity-75 transition"
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black text-center justify-center flex hover:opacity-75 transition"
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full flex items-center justify-center p-2 bg-white cursor-pointer hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full flex items-center justify-center p-2 bg-white cursor-pointer hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default Header;
