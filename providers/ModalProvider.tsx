b68edc0e9e0deec0c1f03e5468b09aebc23df8ce
Tabarak Sohail <tabaraksohail96@gmail.com>
2023-11-14 00:11:48 +0500

    feat: upload-modal and song-upload-on-database-functionlaity-done

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -2,6 +2,7 @@
 
 import AuthModal from "@/components/AuthModal";
 import Modal from "@/components/Modal";
+import UploadModal from "@/components/UploadModal";
 import React, { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -16,6 +17,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   );
 };
